50c452da97b1b5c346feb4e1e1d314be380f8e0e
KS <krisacz@yahoo.com>
2016-05-16 16:54:59 +0100

    ZombieTest initial commit.

---
+++ ZombieTest/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 _offset;
+
+    private void Start ()
+    {
+        _offset = transform.position - Player.transform.position;
+    }
+
+    private void Update ()
+    {
+        transform.position = Player.transform.position + _offset;
+    }
+}
---
+++ ZombieTest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float WalkingSpeed;
+
+    public Rigidbody2D BulletPrefab;
+    public GameObject BulletSpawner;
+    public float FireSpeed = 0.5f;
+    public float FireCoolDown;
+    public float BulletSpeed = 500;
+
+
+    private void Start ()
+    {
+	    
+	}
+    
+    private void Update ()
+	{
+	    Movement();
+	    LookAt();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (!(Time.time >= FireCoolDown)) return;
+        if (Input.GetMouseButton(0)) SpawnBullet();
+    }
+
+    private void SpawnBullet()
+    {
+        var bPrefab = Instantiate(BulletPrefab, new Vector3(BulletSpawner.transform.position.x, BulletSpawner.transform.position.y, BulletSpawner.transform.position.z), Quaternion.identity) as Rigidbody2D;
+        if (bPrefab != null) bPrefab.GetComponent<Rigidbody2D>().AddForce(BulletSpawner.transform.right * BulletSpeed);
+        FireCoolDown = Time.time + FireSpeed;
+    }
+
+    private void LookAt()
+    {
+        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
+        transform.right = direction;
+    }
+
+    private void Movement()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector2(moveHorizontal, moveVertical)*WalkingSpeed;
+        transform.position = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
+    }
+}
